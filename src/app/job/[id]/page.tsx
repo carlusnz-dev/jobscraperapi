1d65c0bb35803989dfba2e29276ee9b8aec92ffb
Carlos Antunes <carlosantunes.dev@gmail.com>
2025-12-11 20:10:22 -0300

    Adição de data de publicação no Model e paginação da pág. inicial

--- src/app/job/[id]/page.tsx
+++ src/app/job/[id]/page.tsx
@@ -13,32 +13,39 @@ export default function JobPage() {
     fetch(`http://localhost:5000/vagas/${params.id}`)
       .then((response) => response.json())
       .then((data) => setJob(data));
-  }, []);
+  }, [params.id]);
 
   return (
     <div className="min-h-screen w-full flex flex-col justify-center items-center">
-      <h1 className="text-4xl font-bold">{job?.title}</h1>
-      <h3 className="text-2xl text-white/40">{job?.company}</h3>
-      <span className="w-1/2 md:w-1/3 my-5 sm:my-8 border border-white/50 rounded-full"></span>
-      <ul className="list-disc">
-        {job?.description.map((item) => (
-          <li key={item}>{item}</li>
-        ))}
-      </ul>
+      <div className="max-w-1/2 flex flex-col items-center text-center">
+        {job?.link.includes('gupy') && (
+          <p className="mb-3 px-3 text-lg text-black bg-white">Gupy</p>
+        )}
 
-      <div className="flex">
-        <Link href={`/`} className="text-white">
-          <p className="mt-12 px-5 py-2 bg-transparent text-white border-2 border-white hover:bg-white hover:text-black transition-all">
-            Voltar
-          </p>
-        </Link>
-        <a
-          className="mt-12 px-5 py-2 bg-white text-black hover:opacity-50 transition-all"
-          href={job?.link}
-          target="_blank"
-        >
-          Ver mais
-        </a>
+        <h1 className="text-4xl font-bold">{job?.title}</h1>
+        <h3 className="text-2xl text-white/40">{job?.company}</h3>
+        <p className="text-lg text-white/80">{job?.date}</p>
+        <span className="w-1/2 md:w-1/3 my-5 sm:my-8 border border-white/50 rounded-full"></span>
+        <ul className="list-decimal">
+          {job?.description.map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
+
+        <div className="flex">
+          <Link href={`/`} className="text-white">
+            <p className="mt-12 px-5 py-2 bg-transparent text-white border-2 border-white hover:bg-white hover:text-black transition-all">
+              Voltar
+            </p>
+          </Link>
+          <a
+            className="mt-12 px-5 py-2 bg-white text-black hover:opacity-50 transition-all"
+            href={job?.link}
+            target="_blank"
+          >
+            Ver mais
+          </a>
+        </div>
       </div>
     </div>
   );
